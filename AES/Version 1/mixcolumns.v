c356b3b8e7e144d352348458cea0d29d42d5fc14
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2020-01-27 19:11:06 +0200

    Adding Comments to each module

--- AES/Version 1/mixcolumns.v
+++ AES/Version 1/mixcolumns.v
@@ -1,4 +1,22 @@
+/*
+------Mix Column Module------
+input : 
+	1- in -128 bit wire- (state)
+output : 
+	1- out -128 bit register- (output)
+Description :
+	- the module consists of 7 functions, one in which it multiplies by 2, 
+	  the 2nd one multiplies by 3 (using modular arithmetic), the 3rd one
+	  multiplies and adds with [02 03 01 01] over GF(2^8), the 4th one
+	  multiplies and adds with [01 02 03 01] over GF(2^8), the 5th one
+	  multiplies and adds with [01 01 02 03] over GF(2^8), the 6th one
+	  multiplies and adds with [03 01 01 02] over GF(2^8), the last one
+	  is the output matrix
+
+*/
+
 module Mix_Column(input wire[127:0]in, output reg[127:0]out);
+
   function [7:0] mul_2(input [7:0] byte);  // multiply by 2 "x" 
     begin 
       if(byte[7]==1'b0)		// if MSB = 0, SLL by 1
@@ -47,10 +65,12 @@ module Mix_Column(input wire[127:0]in, output reg[127:0]out);
       mix_column = {mix_column0(word),mix_column1(word),mix_column2(word),mix_column3(word)};
     end
   endfunction
+  
   always @*
-	  begin
- out = {mix_column(in[127:96]), mix_column(in[95:64]), mix_column(in[63:32]), mix_column(in[31:0])};
-		   end
+   begin
+	out = {mix_column(in[127:96]), mix_column(in[95:64]), mix_column(in[63:32]), mix_column(in[31:0])};
+   end
+		   
 endmodule 
 //
 //128'h7cf22bab6b30767701fe7b6f0763c567
